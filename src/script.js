e6ca6c838cbc7c9b7f444ba07fd6663211823432
Nazrul <appleios986@gmail.com>
2026-04-21 20:37:55 +0530

    I have added the function get weather condition by current locations

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 import getWeatherData from "./getWeatherData.js";
 const searchWeatherForm = document.querySelector("#search-weather");
 const searchByCurrentLocation = document.querySelector("#current-location");
-
 const weatherData = getWeatherData();
 
 searchWeatherForm.addEventListener("submit", searchFormHandler);
@@ -25,6 +24,24 @@ function validateCityName(cityName) {
   }
 }
 
-function searchByCurrentLocationHandler(e) {
-  console.log(e.target);
+function searchByCurrentLocationHandler() {
+  navigator.geolocation.getCurrentPosition((position) => {
+    let data = weatherData.getByCurrentLocation(
+      position.coords.latitude,
+      position.coords.longitude,
+    );
+
+    console.log(data);
+    console.log(
+      data.then((result) => {
+        let forcastData = weatherData.getFiveDaysForecast(result.data.name);
+
+        let airpollutionData = weatherData.getAQIData(
+          result.data.coord.lat,
+          result.data.coord.lon,
+        );
+        console.log(airpollutionData);
+      }),
+    );
+  });
 }
